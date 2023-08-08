f015e0c7be6a0df4d81b1224ea95ea947000e13e
Karolis <karolisdarbinis2@gmail.com>
2023-08-08 10:49:53 +0300

    Nav transition, animations, nav dom js

---
+++ navigation.js
@@ -0,0 +1,14 @@
+const navDOM = document.querySelector(".nav-btn");
+const toggleDOM = document.querySelector(".nav-toggle");
+
+toggleDOM.addEventListener("click", () => {
+  const visibility = navDOM.getAttribute("data-visible");
+  if (visibility === "false") {
+    navDOM.setAttribute("data-visible", true);
+    toggleDOM.setAttribute("aria-expanded", true);
+  } else {
+    navDOM.setAttribute("data-visible", false);
+    toggleDOM.setAttribute("aria-expanded", false);
+  }
+  console.log(visibility);
+});
